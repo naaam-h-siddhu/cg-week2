ca0f4fa894318f79935449f38f429ced44f31c2b
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-24 16:38:54 +0530

    Added Restaurant Management System

--- JavaInheritance/HybridInheritance/RestaurantManagementSystem/Waiter.java
+++ JavaInheritance/HybridInheritance/RestaurantManagementSystem/Waiter.java
@@ -1,4 +1,12 @@
 package RestaurantManagementSystem;
 
-public class Waiter {
+public class Waiter extends Person implements Worker{
+    public Waiter(String name, int id) {
+        super(name, id);
+    }
+    @Override
+    public void performDuties(){
+        System.out.println(super.getName()+" is performing waiter duty");
+    }
 }
+
